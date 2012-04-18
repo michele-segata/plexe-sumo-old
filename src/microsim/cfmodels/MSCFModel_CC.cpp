78279a4a78a782e391eacbc42714f02ee4c32fa1
Michele Segata <segata@ccs.uibk.ac.at>
2012-04-18 16:12:46 +0200

    Adding the car following model for cruise controllers, which includes CC, ACC and CACC. still to be tested

---
+++ src/microsim/cfmodels/MSCFModel_CC.cpp
@@ -0,0 +1,235 @@
+/****************************************************************************/
+/// @file    MSCFModel_CC.cpp
+/// @author  Michele Segata
+/// @date    Wed, 18 Apr 2012
+/// @version $Id: $
+///
+// A series of automatic Cruise Controllers (CC, ACC, CACC)
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright (C) 2001-2011 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include "MSCFModel_CC.h"
+#include <microsim/MSVehicle.h>
+#include <microsim/MSLane.h>
+#include <utils/common/RandHelper.h>
+#include <utils/common/SUMOTime.h>
+
+
+// ===========================================================================
+// method definitions
+// ===========================================================================
+MSCFModel_CC::MSCFModel_CC(const MSVehicleType* vtype,
+                           SUMOReal accel, SUMOReal decel,
+                           SUMOReal ccDecel, SUMOReal headwayTime, SUMOReal constantSpacing,
+                           SUMOReal kp, SUMOReal lambda, SUMOReal c1, SUMOReal xi,
+                           SUMOReal omegaN, SUMOReal tau)
+    : MSCFModel(vtype, accel, decel, headwayTime), myCcDecel(ccDecel), myConstantSpacing(constantSpacing)
+    , myKp(kp), myLambda(lambda), myC1(c1), myXi(xi), myOmegaN(omegaN), myTau(tau), myAlpha1(1 - myC1), myAlpha2(myC1),
+    myAlpha3(-(2 * myXi - myC1 *(myXi + sqrt(myXi* myXi - 1))) * myOmegaN), myAlpha4(-(myXi + sqrt(myXi* myXi - 1)) * myOmegaN* myC1),
+    myAlpha5(-myOmegaN* myOmegaN), myAlpha(DELTA_T / (myTau + DELTA_T), myOneMinusAlpha(1 - myAlpha)) {
+    if (DELTA_T != 100) {
+        std::cerr << "Fatal error: in order to use automatic cruise control models, time step must be set to 100 ms\n";
+        assert(false);
+    }
+}
+
+MSCFModel_CC::~MSCFModel_CC() {}
+
+
+SUMOReal
+MSCFModel_CC::moveHelper(MSVehicle* const veh, SUMOReal vPos) const {
+    const SUMOReal vNext = MSCFModel::moveHelper(veh, vPos);
+    //TODO: understand and change this method
+    return vNext;
+}
+
+
+SUMOReal
+MSCFModel_CC::followSpeed(const MSVehicle* const veh, SUMOReal speed, SUMOReal gap2pred, SUMOReal predSpeed, SUMOReal /*predMaxDecel*/) const {
+    return _v(veh, gap2pred, speed, predSpeed, desiredSpeed(veh));
+}
+
+
+SUMOReal
+MSCFModel_CC::stopSpeed(const MSVehicle* const veh, SUMOReal gap2pred) const {
+    VehicleVariables* vars = (VehicleVariables*)veh->getCarFollowVariables();
+    switch (vars->activeController) {
+        case VehicleVariables::ACC:
+        case VehicleVariables::CACC:
+
+            std::cerr << "Cruise controllers should not be used when a stop can happen, like intersections\n";
+            assert(false);
+            break;
+
+        case VehicleVariables::DRIVER:
+
+            std::cerr << "stopSpeed() correctly invoked for driver, but still not implemented\n";
+            assert(false);
+            break;
+
+        default:
+
+            std::cerr << "Unkown controller selected\n";
+            assert(false);
+            break;
+
+    }
+    if (gap2pred < 0.01) {
+        return 0;
+    }
+    return _v(veh, gap2pred, veh->getSpeed(), 0, desiredSpeed(veh));
+}
+#include <microsim/MSLane.h>
+#include <microsim/MSVehicle.h>
+#include <microsim/MSVehicleType.h>
+#include <utils/xml/SUMOXMLDefinitions.h>
+
+/// @todo update interactionGap logic to IDM
+SUMOReal
+MSCFModel_CC::interactionGap(const MSVehicle* const veh, SUMOReal vL) const {
+
+    //TODO: understand and change this. for now use maximum radar range
+    return 250;
+
+}
+
+
+SUMOReal
+MSCFModel_CC::_v(const MSVehicle* const veh, SUMOReal gap2pred, SUMOReal egoSpeed, SUMOReal predSpeed, SUMOReal desSpeed) const {
+
+    //acceleration computed by the controller
+    double controllerAcceleration;
+
+    VehicleVariables* vars = (VehicleVariables*) veh->getCarFollowVariables();
+
+    //TODO: only for testing
+    //-----------------------------------------------------------
+    if (id.substr(id.size() - 2, 2).compare(".0") == 0) {
+        vars->activeController = VehicleVariables::ACC;
+    } else {
+        vars->activeController = VehicleVariables::CACC;
+    }
+    //-----------------------------------------------------------
+
+    //this function has already been invoked for this timestep
+    if (vars->lastUpdate == MSNet::getInstance()->getCurrentTimeStep()) {
+        return vars->egoSpeed;
+    }
+
+
+    //update variables, first of all
+    vars->egoSpeed = egoSpeed;
+    vars->frontAcceleration = SPEED2ACCEL(predSpeed - vars->frontSpeed);
+    vars->frontDistance = gap2pred;
+    vars->frontSpeed = predSpeed;
+    vars->lastUpdate = MSNet::getInstance()->getCurrentTimeStep();
+
+    switch (vars->activeController) {
+
+        case VehicleVariables::ACC:
+
+            //TODO: modify. for now uses only CC
+            controllerAcceleration = _cc(veh);
+
+            break;
+
+        case VehicleVariables::CACC:
+
+            //TODO: again modify probably range/range-rate controller is needed
+            controllerAcceleration = _cacc(veh);
+
+            break;
+
+        case VehicleVariables::DRIVER:
+
+            std::cerr << "Switching to normal driver behavior still not implemented in MSCFModel_CC\n";
+            assert(false);
+            break;
+
+        default:
+
+            std::cerr << "Invalid controller selected in MSCFModel_CC\n";
+            assert(false);
+            break;
+
+    }
+
+    //compute the actual acceleration applied by the engine
+    vars->egoAcceleration = _actuator(veh, controllerAcceleration);
+
+    //compute the speed from the actual acceleration
+    vars->egoSpeed = ACCEL2SPEED(vars->egoAcceleration);
+
+    return MAX2(SUMOReal(0), vars->egoSpeed);
+}
+
+SUMOReal
+MSCFModel_CC::_cc(const MSVehicle* const veh) const {
+
+    VehicleVariables* vars = (VehicleVariables*) veh->getCarFollowVariables();
+
+    return fmin(myAccel, fmax(-myCcDecel, -myKp * (vars->egoSpeed - vars->ccDesiredSpeed)));
+
+}
+
+SUMOReal
+MSCFModel_CC::_acc(const MSVehicle* const veh) const {
+
+    VehicleVariables* vars = (VehicleVariables*) veh->getCarFollowVariables();
+
+    return fmin(myAccel, fmax(-myDecel, -1.0 / myHeadwayTime * (vars->egoSpeed - vars->frontSpeed + myLambda * (-vars->frontDistance + myHeadwayTime * vars->egoSpeed))));
+
+}
+
+SUMOReal
+MSCFModel_CC::_cacc(const MSVehicle* const veh) const {
+
+    VehicleVariables* vars = (VehicleVariables*) veh->getCarFollowVariables();
+
+    //compute epsilon, i.e., the desired distance error
+    double epsilon = -vars->frontDistance + myConstantSpacing; //NOTICE: error (if any) should already be included in frontDistance
+    //compute epsilon_dot, i.e., the desired speed error
+    double epsilon_dot = vars->egoSpeed - vars->frontSpeed;
+
+    return fmin(myAccel, fmax(-myDecel, myAlpha1 * vars->frontAcceleration + myAlpha2 * vars->leaderAcceleration +
+                              myAlpha3 * epsilon_dot + myAlpha4 * (vars->egoSpeed - vars->leaderSpeed) + myAlpha5 * epsilon));
+
+}
+
+SUMOReal
+MSCFModel_CC::_actuator(const MSVehicle* const veh, SUMOReal acceleration) const {
+
+    VehicleVariables* vars = (VehicleVariables*) veh->getCarFollowVariables();
+
+    return myAlpha * acceleration + myOneMinusAlpha * vars->egoAcceleration;
+
+}
+
+
+MSCFModel*
+MSCFModel_CC::duplicate(const MSVehicleType* vtype) const {
+    return new MSCFModel_CC(vtype,
+                            myAccel, myDecel,
+                            myCcDecel, myHeadwayTime, myConstantSpacing,
+                            myKp, myLambda, myC1, myXi,
+                            myOmegaN, myTau);
+}
---
+++ src/microsim/cfmodels/MSCFModel_CC.h
@@ -0,0 +1,251 @@
+/****************************************************************************/
+/// @file    MSCFModel_CC.h
+/// @author  Michele Segata
+/// @date    Wed, 18 Apr 2012
+/// @version $Id: $
+///
+// A series of automatic Cruise Controllers (CC, ACC, CACC)
+/****************************************************************************/
+// SUMO, Simulation of Urban MObility; see http://sumo.sourceforge.net/
+// Copyright (C) 2001-2011 DLR (http://www.dlr.de/) and contributors
+/****************************************************************************/
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; either version 2 of the License, or
+//   (at your option) any later version.
+//
+/****************************************************************************/
+#ifndef MSCFMODEL_CC_H
+#define	MSCFMODEL_CC_H
+
+// ===========================================================================
+// included modules
+// ===========================================================================
+#ifdef _MSC_VER
+#include <windows_config.h>
+#else
+#include <config.h>
+#endif
+
+#include <microsim/MSCFModel.h>
+#include <microsim/MSLane.h>
+#include <microsim/MSVehicle.h>
+#include <microsim/MSVehicleType.h>
+#include <utils/xml/SUMOXMLDefinitions.h>
+
+
+// ===========================================================================
+// class definitions
+// ===========================================================================
+/** @class MSCFModel_CC
+ * @brief A set of automatic Cruise Controllers, including classic Cruise
+ * Control (CC), Adaptive Cruise Control (ACC) and Cooperative Adaptive Cruise
+ * Control (CACC). Take as references the chapters 5, 6 and 7 of the Rajamani's
+ * book "Vehicle dynamics and control" (2011).
+ * @see MSCFModel
+ */
+class MSCFModel_CC : public MSCFModel {
+public:
+    /** @brief Constructor
+     * @param[in] accel The maximum acceleration that controllers can output (def. 1.5 m/s^2)
+     * @param[in] decel The maximum deceleration that ACC and CACC controllers can output (def. 6 m/s^2)
+     * @param[in] ccDecel The maximum deceleration that the CC can output (def. 1.5 m/s^2)
+     * @param[in] headwayTime the headway gap for ACC (be aware of instabilities) (def. 1.5 s)
+     * @param[in] constantSpacing the constant gap for CACC (def. 5 m)
+     * @param[in] kp design constant for CC (def. 1)
+     * @param[in] lambda design constant for ACC (def. 0.1)
+     * @param[in] c1 design constant for CACC (def. 0.5)
+     * @param[in] xi design constant for CACC (def. 1)
+     * @param[in] omegaN design constant for CACC (def. 0.2)
+     * @param[in] tau engine time constant used for actuation lag (def. 0.5 s)
+     */
+    MSCFModel_CC(const MSVehicleType* vtype, SUMOReal accel, SUMOReal decel,
+                 SUMOReal ccDecel, SUMOReal headwayTime, SUMOReal constantSpacing,
+                 SUMOReal kp, SUMOReal lambda, SUMOReal c1, SUMOReal xi,
+                 SUMOReal omegaN, SUMOReal tau);
+
+    /// @brief Destructor
+    ~MSCFModel_CC();
+
+
+    /// @name Implementations of the MSCFModel interface
+    /// @{
+
+    /** @brief Applies interaction with stops and lane changing model influences
+     * @param[in] veh The ego vehicle
+     * @param[in] vPos The possible velocity
+     * @return The velocity after applying interactions with stops and lane change model influences
+     */
+    SUMOReal moveHelper(MSVehicle* const veh, SUMOReal vPos) const;
+
+
+    /** @brief Computes the vehicle's safe speed (no dawdling)
+     * @param[in] veh The vehicle (EGO)
+     * @param[in] speed The vehicle's speed
+     * @param[in] gap2pred The (netto) distance to the LEADER
+     * @param[in] predSpeed The speed of LEADER
+     * @return EGO's safe speed
+     * @see MSCFModel::ffeV
+     */
+    SUMOReal followSpeed(const MSVehicle* const veh, SUMOReal speed, SUMOReal gap2pred, SUMOReal predSpeed, SUMOReal predMaxDecel) const;
+
+
+    /** @brief Computes the vehicle's safe speed for approaching a non-moving obstacle (no dawdling)
+     * @param[in] veh The vehicle (EGO)
+     * @param[in] gap2pred The (netto) distance to the the obstacle
+     * @return EGO's safe speed for approaching a non-moving obstacle
+     * @see MSCFModel::ffeS
+     * @todo generic Interface, models can call for the values they need
+     */
+    SUMOReal stopSpeed(const MSVehicle* const veh, SUMOReal gap2pred) const;
+
+
+    /** @brief Returns the maximum gap at which an interaction between both vehicles occurs
+     *
+     * "interaction" means that the LEADER influences EGO's speed.
+     * @param[in] veh The EGO vehicle
+     * @param[in] vL LEADER's speed
+     * @return The interaction gap
+     * @todo evaluate signature
+     * @see MSCFModel::interactionGap
+     */
+    SUMOReal interactionGap(const MSVehicle* const , SUMOReal vL) const;
+
+
+    /** @brief Returns the model's name
+     * @return The model's name
+     * @see MSCFModel::getModelName
+     */
+    int getModelID() const {
+        return SUMO_TAG_CF_CC;
+    }
+    /// @}
+
+
+
+    /** @brief Duplicates the car-following model
+     * @param[in] vtype The vehicle type this model belongs to (1:1)
+     * @return A duplicate of this car-following model
+     */
+    MSCFModel* duplicate(const MSVehicleType* vtype) const;
+
+
+    VehicleVariables* createVehicleVariables() const {
+        return new VehicleVariables();
+    }
+
+    void setCCDesiredSpeed(const MSVehicle* veh, SUMOReal ccDesiredSpeed);
+
+    void setLeaderInformation(const MSVehicle* veh, SUMOReal speed, SUMOReal acceleration);
+
+
+private:
+    class VehicleVariables : public MSCFModel::VehicleVariables {
+    public:
+        VehicleVariables() : egoSpeed(0), egoAcceleration(0), frontSpeed(0), frontDistance(0), leaderSpeed(0),
+            leaderAcceleration(0), platoonId(""), isPlatoonLeader(false), ccDesiredSpeed(14), activeController(ACC), lastUpdate(0) {}
+
+        /** @enum ActiveController
+         * @brief Determines the currently active controller, i.e., ACC, CACC, or the
+         * driver. In future we might need to switch off the automatic controller and
+         * leave the control to the mobility model which reproduces a human driver
+         */
+        enum ActiveController
+        {ACC, CACC, DRIVER};
+
+        /// @brief current vehicle speed
+        SUMOReal egoSpeed;
+        /// @brief current vehicle acceleration
+        SUMOReal egoAcceleration;
+        /// @brief current front vehicle speed
+        SUMOReal frontSpeed;
+        /// @brief current front vehicle distance
+        SUMOReal frontDistance;
+        /// @brief current front vehicle acceleration (used by CACC)
+        SUMOReal frontAcceleration;
+        /// @brief last timestep at which front vehicle data has been updated
+        SUMOTime lastUpdate;
+        /// @brief platoon's leader speed (used by CACC)
+        SUMOReal leaderSpeed;
+        /// @brief platoon's leader acceleration (used by CACC)
+        SUMOReal leaderAcceleration;
+
+        /// @brief CC desired speed
+        SUMOReal ccDesiredSpeed;
+        /// @brief currently active controller
+        enum ActiveController activeController;
+
+        //TODO: most probably the following variables needs to be moved to the application logic (i.e., network protocol)
+        /// @brief own platoon id
+        std::string platoonId;
+        /// @brief is ego vehicle the leader?
+        bool isPlatoonLeader;
+    };
+
+
+private:
+    SUMOReal _v(const MSVehicle* const veh) const;
+
+    /// @brief controller for the CC which computes the acceleration to be applied. the value needs to be passed to the actuator
+    SUMOReal _cc(const MSVehicle* const veh) const;
+
+    /// @brief controller for the ACC which computes the acceleration to be applied. the value needs to be passed to the actuator
+    SUMOReal _acc(const MSVehicle* const vehd) const;
+
+    /// @brief controller for the CACC which computes the acceleration to be applied. the value needs to be passed to the actuator
+    SUMOReal _cacc(const MSVehicle* const veh) const;
+
+    /// @brief computes the actual acceleration the actuator is able to apply to the car, given engine time constant and previous
+    /// acceleration
+    SUMOReal _actuator(const MSVehicle* const veh, SUMOReal acceleration, SUMOReal previousAcceleration) const;
+
+    SUMOReal desiredSpeed(const MSVehicle* const veh) const {
+        return MIN2(myType->getMaxSpeed(), veh->getLane()->getMaxSpeed());
+    }
+
+
+private:
+
+    /// @brief The maximum deceleration that the CC can output
+    const SUMOReal myCcDecel;
+
+    /// @brief the constant gap for CACC
+    const SUMOReal myConstantSpacing;
+
+    /// @brief design constant for CC
+    const SUMOReal myKp;
+
+    /// @brief design constant for ACC
+    const SUMOReal myLambda;
+
+    /// @brief design constant for CACC
+    const SUMOReal myC1;
+
+    /// @brief design constant for CACC
+    const SUMOReal myXi;
+
+    /// @brief design constant for CACC
+    const SUMOReal myOmegaN;
+
+    /// @brief engine time constant used for actuation lag
+    const SUMOReal myTau;
+    /// @brief the alpha parameter for the low-pass filter used to implement the actuation lag
+    const SUMOReal myAlpha;
+    /// @brief one minus alpha parameters
+    const SUMOReal myOneMinusAlpha;
+
+    /// @brief A computational shortcut for CACC
+    const SUMOReal myAlpha1;
+    /// @brief A computational shortcut for CACC
+    const SUMOReal myAlpha2;
+    /// @brief A computational shortcut for CACC
+    const SUMOReal myAlpha3;
+    /// @brief A computational shortcut for CACC
+    const SUMOReal myAlpha4;
+    /// @brief A computational shortcut for CACC
+    const SUMOReal myAlpha5;
+
+};
+
+#endif	/* MSCFMODEL_CC_H */
